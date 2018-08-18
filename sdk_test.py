beff182ded1581cd38033ee2363a6bd75e6d39ef
adam <ahanna@alumni.mines.edu>
2018-08-17 22:13:17 -0700

    all tests passing

--- sdk_test.py
+++ sdk_test.py
@@ -1,6 +1,7 @@
 from ctypes import *
 import unittest
 import sdk
+import json
 
 hexutil = CDLL('./lib/hexutil/hexutil.so')
 
@@ -29,6 +30,48 @@ class TestSDK(unittest.TestCase):
 
         self.assertEqual(expectKey, key)
         self.assertEqual(expectVal, val)
+
+    def test_store(self):
+        key = "foo"
+        val = "bar"
+
+        c3.state[key] = val
+
+        self.assertEqual(c3.state[key], val)
+        del c3.state[key]
+
+    def test_state(self):
+        methodName = "setState"
+
+        key1 = "foo"
+        val1 = "bar"
+
+        key2 = "foofoo"
+        val2 = "barbar"
+
+        def setState(k, v):
+            c3.state[k] = v
+
+        c3.registerMethod(methodName, setState)
+
+        p1 = [
+            methodName,
+            c_char_p(hexutil.EncodeString(c_char_p(key1.encode('utf-8')))).value.decode('utf-8'),
+            c_char_p(hexutil.EncodeString(c_char_p(val1.encode('utf-8')))).value.decode('utf-8'),
+        ]
+        p2 = [
+            methodName,
+            c_char_p(hexutil.EncodeString(c_char_p(key2.encode('utf-8')))).value.decode('utf-8'),
+            c_char_p(hexutil.EncodeString(c_char_p(val2.encode('utf-8')))).value.decode('utf-8'),
+        ]
+
+        params = [p1, p2]
+        paramsJSON = json.dumps(params)
+
+        c3.process(paramsJSON)
+
+        self.assertEqual(c3.state[key1], val1)
+        self.assertEqual(c3.state[key2], val2)
 
 if __name__ == '__main__':
     unittest.main()
